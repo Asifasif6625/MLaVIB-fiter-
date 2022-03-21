89b3c01441cba2efbfb0914662ed0a1d5599a154
Asif A.S <86577020+Asifasif6625@users.noreply.github.com>
2022-03-21 13:21:44 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -26,7 +26,7 @@ async def answer(bot, query):
     if not await inline_users(query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='okDa',
+                           switch_pm_text='athu veenda... 👮‍♂️💬',
                            switch_pm_parameter="hehe")
         return
 
